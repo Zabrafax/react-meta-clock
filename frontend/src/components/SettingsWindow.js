15921195e9e38c00c6dc441f7a1363f87a70ea4d
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-06-29 01:58:43 +0200

    settings window improvement

--- frontend/src/components/SettingsWindow.js
+++ frontend/src/components/SettingsWindow.js
@@ -3,6 +3,7 @@ import './SettingsWindow.css';
 function SettingsWindow() {
     return (
         <div className="Settings__window">
+            <h2>Settings</h2>
             <p>Settings here!</p>
         </div>
     );
