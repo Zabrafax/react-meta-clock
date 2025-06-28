9ffd037910abb60c55e84901178751c620bc9c56
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-06-29 01:16:04 +0200

    nice header buttons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,6 +6,10 @@ function Header() {
             <div className="Header__logo">
                 <h1>Meta Clock</h1>
             </div>
+            <div className="Header__buttons">
+                <a className="Header__a__button">SETTINGS</a>
+                <a className="Header__a__button">ACCOUNT</a>
+            </div>
         </header>
     );
 }
