6be264eaa056836100f94e062fe0883e92deca86
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-07-01 01:03:06 +0200

    setting switch line width added

--- frontend/src/components/SimpleSwitch.js
+++ frontend/src/components/SimpleSwitch.js
@@ -1,17 +1,47 @@
 import './SimpleSwitch.css'
+import { useState, useEffect, useRef } from 'react';
 
 function SimpleSwitch({ name, onEnable, onDisable } ) {
+    const [lineWidth, setLineWidth] = useState(0);
+    const [isEnabled, setIsEnabled] = useState(true);
+
+    const enableRef = useRef(null);
+    const disableRef = useRef(null);
+
+    useEffect(() => {
+        if (enableRef.current && disableRef.current) {
+            const enableWidth = enableRef.current.getBoundingClientRect().width;
+            const disableWidth = disableRef.current.getBoundingClientRect().width;
+
+            setLineWidth(!!isEnabled ? enableWidth : disableWidth);
+        }
+    }, [isEnabled]);
+
+    const enable = () => {
+        onEnable();
+        setIsEnabled(true);
+    }
+
+    const disable = () => {
+        onDisable();
+        setIsEnabled(false);
+    }
+
     return (
         <div className="Simple__switch">
-            <p>{name}</p>
+            <p>{name + ": "}</p>
             <div className="Choices__wrapper">
                 <div className="Choices__text">
-                    <p>[</p>
-                    <a onClick={onEnable}>Enable</a>
-                    <p>|</p>
-                    <a onClick={onDisable}>Disable</a>
+                    <p>[ </p>
+                    <a ref={enableRef} onClick={enable}>Enable</a>
+                    <p> | </p>
+                    <a ref={disableRef} onClick={disable}>Disable</a>
                     <p>]</p>
                 </div>
+                <div
+                    className="Choices__line"
+                    style={{width: lineWidth + "px"}}
+                ></div>
             </div>
         </div>
     );
