a50b160bba6b881c59edcd917741af697d533205
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-07-12 15:45:59 +0200

    error window created

--- frontend/src/components/windows/ServerErrorWindow.js
+++ frontend/src/components/windows/ServerErrorWindow.js
@@ -1,19 +1,22 @@
 import "./Window.css";
 import "./ServerErrorWindow.css";
+import "../Buttons/SmallAButton.css"
 
 function ServerErrorWindow(onServerErrorCrossClick) {
     return (
         <div className="Server__error__window Window">
             <div className="Window__top__wrapper">
-                <h2>Error</h2>
+                <h2>Connection Error</h2>
                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <a className="Closing__cross" onClick={onServerErrorCrossClick}>
+                <a className="Closing__cross" onClick={() => window.location.reload()}>
                     <div className="Closing_cross__first"></div>
                     <div className="Closing_cross__second"></div>
                 </a>
             </div>
-            <div className="Window__main__wrapper">
-                <p>There is an error </p>
+            <div className="Window__main__wrapper Server__error__window__main__wrapper">
+                <p>Unable to connect to the server</p>
+                <p>Please check your internet connection or try again later :3</p>
+                <a className="Small__a__button" onClick={() => window.location.reload()}>Reload page</a>
             </div>
         </div>
     );
