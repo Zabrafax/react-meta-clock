ef8facf5f28d1dc8e60d76708b1886ee26906c57
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-07-18 22:57:47 +0200

    idk

--- frontend/src/components/buttons/TimeZonePicker.js
+++ frontend/src/components/buttons/TimeZonePicker.js
@@ -1,11 +1,27 @@
 import styles from './TimeZonePicker.module.css';
 import {useTimeZones} from "../contexts/TimeZoneContext";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
+import {useTheme} from "../contexts/ThemeContext";
 
 function TimeZonePicker() {
+    const { allFirstThemeColors, allTextThemeColors, currentThemeNumber } = useTheme();
+
     const { timeZones, currentTimeZoneId, setCurrentTimeZoneId } = useTimeZones();
 
     const [isOpen, setIsOpen] = useState(false);
+    const textRef = useRef();
+    const [isFocused, setIsFocused] = useState(false);
+    const [lineWidth, setLineWidth] = useState(0);
+
+    function enableLine() {
+        if (textRef.current) {
+            setLineWidth(textRef.current.getBoundingClientRect().width);
+        }
+    }
+
+    function disableLine() {
+        setLineWidth(0);
+    }
 
     function handleTimeZoneChange(event) {
         setCurrentTimeZoneId(event.target.value);
@@ -13,9 +29,18 @@ function TimeZonePicker() {
 
     return (
         <div className={styles.TimeZonePicker}>
-            <div className={styles.Text__wrapper}>
+            <div className={styles.Text__wrapper} ref={textRef} onMouseEnter={enableLine} onMouseLeave={disableLine}>
                 <p>{currentTimeZoneId ? timeZones.find(tz => tz.id === currentTimeZoneId)?.label : 'Choose timezone'}</p>
                 <span className={styles.Arrow}>{isOpen ? '▲' : '▼'}</span>
+            </div>
+            <div
+                className={styles.Bottom__line}
+                style={{
+                    backgroundColor: allTextThemeColors[currentThemeNumber],
+                    width: lineWidth + 'px'
+                }}
+            >
+
             </div>
         </div>
 
