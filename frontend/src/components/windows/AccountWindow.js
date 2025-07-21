9184009f6c4599704f7f7e023a727ea81f4a4f2c
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-07-21 22:39:42 +0200

    account window form started

--- frontend/src/components/windows/AccountWindow.js
+++ frontend/src/components/windows/AccountWindow.js
@@ -1,9 +1,12 @@
 import {useTheme} from "../contexts/ThemeContext";
 import {HEXtoRGBA} from "../utils/colorUtils";
 import './AccountWindow.css';
+import {useState} from "react";
 
 function AccountWindow({ onAccountCrossClick }) {
     const { allFirstThemeColors, allAccentThemeColors, allTextThemeColors, allAlphaThemePercents, currentThemeNumber } = useTheme();
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     return (
         <div
@@ -21,9 +24,17 @@ function AccountWindow({ onAccountCrossClick }) {
                     style={{ "--after-color": allTextThemeColors[currentThemeNumber] }}
                 ></a>
             </div>
-            <div className="Window__main__wrapper Account__window__main__wrapper">
-
-            </div>
+            {!isLoggedIn &&
+                <div className="Window__main__wrapper Account__window__login__wrapper">
+                    <h1>Login</h1>
+                    <div className="Account__window__login__form__wrapper">
+                        <form className="Account__window__login__form">
+                            <input type="text" className="Account__window__login__form__input" placeholder="Username" />
+                            <input type="password" className="Account__window__login__form__input" placeholder="Password" />
+                        </form>
+                    </div>
+                </div>
+            }
         </div>
     );
 }
