b90bf4c13d074a81251d01c545554bec2217a5ec
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-07-26 23:14:17 +0200

    ref

--- frontend/src/components/buttons/ThemeSwitch.js
+++ frontend/src/components/buttons/ThemeSwitch.js
@@ -40,7 +40,7 @@ const ThemeSwitch = forwardRef(({ name, choiceColors, lineColors }, ref) => {
             <p>{name + ": "}</p>
             <div className={styles.Choices__wrapper}>
                 <div className={styles.Choice__options}>
-                    {choiceColors.map((color, index) => (
+                    {choiceColors.slice(0, -1).map((color, index) => (
                         // eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/anchor-is-valid
                         <a
                             key={index}
@@ -52,9 +52,9 @@ const ThemeSwitch = forwardRef(({ name, choiceColors, lineColors }, ref) => {
                     ))}
                     <a
                         key={"custom"}
-                        ref={el => (colorRefs.current[choiceColors.length] = el)}
+                        ref={el => (colorRefs.current[choiceColors.length - 1] = el)}
                         className={styles.Choice__option__custom}
-                        onClick={() => setCurrentThemeNumber(choiceColors.length)}
+                        onClick={() => setCurrentThemeNumber(choiceColors.length - 1)}
                     ></a>
                 </div>
                 <div className={styles.Choices__line__wrapper}>
--- frontend/src/components/contexts/ThemeContext.js
+++ frontend/src/components/contexts/ThemeContext.js
@@ -6,7 +6,7 @@ export function ThemeProvider({ children }) {
     const [allFirstThemeColors, setAllFirstThemeColors] = useState(["#1d2d65", "#eaeaea", "#af0e0e", "#62d59f", "#1d2d65"]);
     const [allSecondThemeColors, setAllSecondThemeColors] = useState(["#162652", "#d2d2d2", "#960e0e", "#54be90", "#162652"]);
     const [allAccentThemeColors, setAllAccentThemeColors] = useState(["#dcdcdc", "#000000", "#ffd700", "#ffffff", "#dcdcdc"]);
-    const [allTextThemeColors, setAllTextThemeColors] = useState(["#ffffff", "#000000", "#ffffff", "#000000"], "#ffffff");
+    const [allTextThemeColors, setAllTextThemeColors] = useState(["#ffffff", "#000000", "#ffffff", "#000000", "#ffffff"]);
     const [allAlphaThemePercents, setAllAlphaThemePercents] = useState([0.85, 0.85, 0.8, 0.8, 0.85]);
     const [currentThemeNumber, setCurrentThemeNumber] = useState(0);
 
