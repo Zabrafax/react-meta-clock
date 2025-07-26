83e12d20cc21b119e7c38bcee1a3f77a710bb884
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-07-26 23:24:46 +0200

    ref

--- frontend/src/components/buttons/ThemeSwitch.js
+++ frontend/src/components/buttons/ThemeSwitch.js
@@ -4,16 +4,16 @@ import {useTheme} from "../contexts/ThemeContext";
 
 const ThemeSwitch = forwardRef(({ name, choiceColors, lineColors }, ref) => {
 // function ThemeSwitch({ name, choiceColors, lineColors }) {
-    const { currentThemeNumber, setCurrentThemeNumber, customAccentThemeColor } = useTheme();
-    const customThemeNumber = choiceColors.length;
+    const { currentThemeNumber, setCurrentThemeNumber } = useTheme();
 
     const [lineColor, setLineColor] = useState(0);
 
     const [leftLineOffset, setLeftLineOffset] = useState(0);
     const [lineWidth, setLineWidth] = useState(0);
 
+    const isCustomColor = currentThemeNumber === choiceColors.length - 1;
+
     const colorRefs = useRef([]);
-    const customColors = useRef(null);
 
     useEffect(() => {
         if(colorRefs.current[0]) {
@@ -23,11 +23,7 @@ const ThemeSwitch = forwardRef(({ name, choiceColors, lineColors }, ref) => {
             setLineWidth(basicLineWidth);
         }
 
-        if(currentThemeNumber === customThemeNumber) {
-            setLineColor(customAccentThemeColor);
-        } else {
-            setLineColor(lineColors[currentThemeNumber]);
-        }
+        setLineColor(lineColors[currentThemeNumber]);
 
     }, [currentThemeNumber, choiceColors, colorRefs, lineColors, setCurrentThemeNumber]);
 
@@ -37,37 +33,48 @@ const ThemeSwitch = forwardRef(({ name, choiceColors, lineColors }, ref) => {
 
     return (
         <div ref={ref} className={styles.ThemeSwitch}>
-            <p>{name + ": "}</p>
-            <div className={styles.Choices__wrapper}>
-                <div className={styles.Choice__options}>
-                    {choiceColors.slice(0, -1).map((color, index) => (
-                        // eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/anchor-is-valid
+            <div className={styles.Horizontal__wrapper}>
+                <p>{name + ": "}</p>
+                <div className={styles.Choices__wrapper}>
+                    <div className={styles.Choice__options}>
+                        {choiceColors.slice(0, -1).map((color, index) => (
+                            // eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/anchor-is-valid
+                            <a
+                                key={index}
+                                ref={el => (colorRefs.current[index] = el)}
+                                className={styles.Choice__option}
+                                style={{ backgroundColor: color }}
+                                onClick={() => setCurrentThemeNumber(index)}
+                            ></a>
+                        ))}
                         <a
-                            key={index}
-                            ref={el => (colorRefs.current[index] = el)}
-                            className={styles.Choice__option}
-                            style={{ backgroundColor: color }}
-                            onClick={() => setCurrentThemeNumber(index)}
+                            key={"custom"}
+                            ref={el => (colorRefs.current[choiceColors.length - 1] = el)}
+                            className={styles.Choice__option__custom}
+                            onClick={() => setCurrentThemeNumber(choiceColors.length - 1)}
                         ></a>
-                    ))}
-                    <a
-                        key={"custom"}
-                        ref={el => (colorRefs.current[choiceColors.length - 1] = el)}
-                        className={styles.Choice__option__custom}
-                        onClick={() => setCurrentThemeNumber(choiceColors.length - 1)}
-                    ></a>
-                </div>
-                <div className={styles.Choices__line__wrapper}>
-                    <div
-                        className={styles.Choices__line}
-                        style={{
-                            width: lineWidth + "px",
-                            left: leftLineOffset + "px",
-                            backgroundColor: lineColor
-                        }}
-                    />
+                    </div>
+                    <div className={styles.Choices__line__wrapper}>
+                        <div
+                            className={styles.Choices__line}
+                            style={{
+                                width: lineWidth + "px",
+                                left: leftLineOffset + "px",
+                                backgroundColor: lineColor
+                            }}
+                        />
+                    </div>
                 </div>
             </div>
+
+            {isCustomColor &&
+                <div
+                    className={styles.Custom__color__extension}
+                    // style={{ transform: `scaleY(${isCustomColor ? 1 : 0})` }}
+                >
+
+                </div>
+            }
         </div>
     );
 });
