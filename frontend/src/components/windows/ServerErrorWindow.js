811a1032c3174fe54ec51d2a8b806886b11a0f9b
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-07-29 21:36:39 +0200

    ref

--- frontend/src/components/windows/ServerErrorWindow.js
+++ frontend/src/components/windows/ServerErrorWindow.js
@@ -5,13 +5,13 @@ import {useTheme} from "../contexts/ThemeContext";
 import {HEXtoRGBA} from "../utils/colorUtils";
 
 function ServerErrorWindow() {
-    const { allFirstThemeColors, allTextThemeColors, allAlphaThemePercents, currentThemeNumber } = useTheme();
+    const { firstThemeColor, textThemeColor, alphaThemePercent } = useTheme();
 
     return (
         <div
             className="Server__error__window Window"
             style={{
-                backgroundColor: HEXtoRGBA(allFirstThemeColors[currentThemeNumber], allAlphaThemePercents[currentThemeNumber])
+                backgroundColor: HEXtoRGBA(firstThemeColor, alphaThemePercent)
             }}
         >
             <div className="Window__top__wrapper">
@@ -20,7 +20,7 @@ function ServerErrorWindow() {
                 <a
                     className="Closing__cross"
                     onClick={() => window.location.reload()}
-                    style={{ "--after-color": allTextThemeColors[currentThemeNumber] }}
+                    style={{ "--after-color": textThemeColor }}
                 ></a>
             </div>
             <div className="Window__main__wrapper Server__error__window__main__wrapper">
@@ -28,7 +28,7 @@ function ServerErrorWindow() {
                 <p>Please check your internet connection or try again later :3</p>
                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                 <a
-                    style={{ "--after-color": allTextThemeColors[currentThemeNumber] }}
+                    style={{ "--after-color": textThemeColor }}
                     className="Small__a__button"
                     onClick={() => window.location.reload()}
                 >Reload page</a>
