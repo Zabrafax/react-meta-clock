0b464637dede1d20d360abdf73e50dd68740f429
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-08-06 23:36:52 +0200

    cors2

--- backend/src/main/java/com/metaclock/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/metaclock/backend/config/SecurityConfig.java
@@ -28,14 +28,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://zabrafax.github.io"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
+        config.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
         return source;
     }
 }
