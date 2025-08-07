842ab6494614dc151755b28128379c02d2d3821f
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-08-07 22:40:02 +0200

    fixed security config

--- backend/src/main/java/com/metaclock/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/metaclock/backend/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of("https://zabrafax.github.io", "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(false);
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
