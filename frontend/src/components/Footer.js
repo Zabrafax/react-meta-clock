a8fa949321710b5e1a30f27dfc10e9230232d0f4
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-08-08 23:59:26 +0200

    link added to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -12,7 +12,8 @@ function Footer() {
             style={{ backgroundColor: secondThemeColor }}
         >
             <p>Meta Clock 2025</p>
-            <p className="Footer__small__p__text">Inspired by ClockClock 24</p>
+            <p className="Footer__small__p__text">Inspired by</p>
+            <a href="https://www.humanssince1982.com/en-eu/products/clockclock-24-white">ClockClock 24</a>
         </footer>
     );
 }
