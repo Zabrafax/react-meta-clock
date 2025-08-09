a0b4b9d69aa9f2128fe5b7cf72215c27414746e3
Aliaksandr Semutsenka <semutenko182@gmail.com>
2025-08-09 02:39:06 +0200

    footer link fix

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     /*
         Theme
      */
-    const { secondThemeColor } = useTheme();
+    const { secondThemeColor, textThemeColor } = useTheme();
 
     return (
         <footer
@@ -14,7 +14,10 @@ function Footer() {
             <p>Meta Clock 2025</p>
             <div className="Footer__bottom__wrapper">
                 <p className="Footer__small__p__text">Inspired by </p>
-                <a href="https://www.humanssince1982.com/en-eu/products/clockclock-24-white">ClockClock 24</a>
+                <a
+                    href="https://www.humanssince1982.com/en-eu/products/clockclock-24-white"
+                    style={{ "--after-color": textThemeColor }}
+                >ClockClock 24</a>
             </div>
         </footer>
     );
